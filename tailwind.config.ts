21e1c736a3a5a819098578b1d3f3c524e9662c70
Fullstack-Duck <nrsandrin@gmail.com>
2025-05-06 19:43:54 -0300

    tailwind config set

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,12 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{css,scss,html}',
+  ],
   prefix: "",
   theme: {
     container: {
